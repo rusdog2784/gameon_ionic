a64f76b04cdd4df7d7f9ab631f887f550fc4fb1b
Scott Russell <scott.tkdmaster@mac.com>
2017-08-22 15:33:42 -0400

    Initial Commit
    Project set up and CreateGamePage started.

---
+++ src/pages/createGame/createGame.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-createGame',
+    templateUrl: 'createGame.html'
+})
+export class CreateGamePage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
